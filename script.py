9c903f7ec1384294ed8e9eddf383f17632176e25
Sergio <sherrera725@csu.fullerton.edu>
2021-02-03 02:41:36 -0800

    altered script.py to reflect current site's status

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+from datetime import datetime
 from time import sleep
 from dotenv import load_dotenv
-import requests, os, codecs
+import requests, os, codecs, itertools
 load_dotenv()
 
 #==========GLOBAL VARIABLES==========#
@@ -17,15 +18,12 @@ AGE_RANGE = '65 - 74'
 HIT_RESPONSE = "Found appointment(s) available. Text notification sent."
 MISS_RESPONSE = "no appointments yet"
 SHORT_PAUSE = 2
-LONGER_PAUSE = 3
+LONG_PAUSE = 3
+LONGER_PAUSE = 7
 
 def get_driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    chrome_driver = webdriver.Chrome()
+    return chrome_driver
 
 def launch_page(chrome_driver):
     return chrome_driver.get(START_URL)
@@ -41,7 +39,7 @@ def check_availability(CHECKSUM=0):
     launch_page(chrome_driver)
     browser_title_text = 'CDPH'
     assert browser_title_text in chrome_driver.title
-    sleep(LONGER_PAUSE)
+    sleep(LONG_PAUSE)
     
     elem_tag_name = 'button'
     title_btn = chrome_driver.find_element_by_tag_name(elem_tag_name)
@@ -52,7 +50,7 @@ def check_availability(CHECKSUM=0):
     #...SCREENING PAGE
     browser_title_text = 'Screening'
     assert browser_title_text in chrome_driver.title
-    sleep(LONGER_PAUSE)
+    sleep(LONG_PAUSE)
     
     elem_name = "q-screening-18-yr-of-age"
     chrome_driver.find_element_by_name(elem_name).click()
@@ -63,15 +61,17 @@ def check_availability(CHECKSUM=0):
     county_opt = Select(chrome_driver.find_element_by_id(elem_id))
     county_opt.select_by_visible_text(COUNTY)
     
-    xpath = "//input[@name='q-screening-healthworker'][@value='No']"
-    chrome_driver.find_element_by_xpath(xpath).click()
     xpath = "//input[@name='q-screening-eligibility-age-range'][@value='{}']".format(AGE_RANGE)
     chrome_driver.find_element_by_xpath(xpath).click()
+    elem_id = "q-screening-eligibility-industry"
+    work_industry_opt = Select(chrome_driver.find_element_by_id(elem_id))
+    work_industry_opt.select_by_visible_text("Other")
+    
     
     xpath = "//button[@type='submit']"
     chrome_driver.find_element_by_xpath(xpath).click()
     CHECKSUM += 1
-    sleep(LONGER_PAUSE)
+    sleep(LONG_PAUSE)
     
     #...LOCATION SEARCH PAGE
     xpath = "//input[@id='location-search-input']"
@@ -80,47 +80,82 @@ def check_availability(CHECKSUM=0):
     xpath = "//button[text()='Continue']"
     chrome_driver.find_element_by_xpath(xpath).click()
     CHECKSUM += 1
-    sleep(LONGER_PAUSE)
+    sleep(LONG_PAUSE)
     
     #...LOCATION SELECT PAGE
-    elem_tag_name = "button"
-    locations_btn_list = chrome_driver.find_elements_by_tag_name(elem_tag_name)
-    print("location list contains {} locations".format(len(locations_btn_list)))
-    locations_btn_list[len(locations_btn_list)-1].click()
-    CHECKSUM += 1
-    sleep(LONGER_PAUSE)
-    
-    #...APPOINTMENT SELECT PAGE
-    elem_css_selector = 'h3'
-    if chrome_driver.find_elements_by_css_selector(elem_css_selector):
-        CHECKSUM += 1
-        elem_property = "innerHTML"
-        appts_avail_text = chrome_driver.find_element_by_tag_name(elem_css_selector).get_property(elem_property)
-        num_appts = int(appts_avail_text[0]) # appointments number count
-    
-        curr_URL = chrome_driver.current_url # current window's URL
-        print(curr_URL)
-        
-        # send text to notify appointments available to schedule
-        if num_appts > 0:
-            resp = requests.post(
-                TEXTBELT_URL, {
-                    'phone': PHONE_NUM,
-                    'message': curr_URL,
-                    'key': TXT_API_KEY,
-                }
-            )
-            print(HIT_RESPONSE)
-            CHECKSUM += 1
-        else:
-            print(MISS_RESPONSE)
-            CHECKSUM += 1
-        sleep(LONGER_PAUSE)
-    
-    print("CHECKSUM is {}".format(CHECKSUM))
-    assert CHECKSUM >= 5
-    
+    elem_css_selector = "h2.tw-text-n800.tw-text-xl"
+    dose_num_list = None
+    if "No appointments" in chrome_driver.find_elements_by_css_selector(elem_css_selector).text:
+        curr_datetime = datetime.now()
+        print("Still no appointments. Currently: " + curr_datetime.strftime("%Y-%m-%d %H:%M:%S"))
+    else:
+        message = "Appointments opened up." + chrome_driver.current_url
+        resp = requests.post(
+            TEXTBELT_URL, {
+                'phone': PHONE_NUM,
+                'message': message,
+                'key': TXT_API_KEY,
+            }
+        )
     chrome_driver.close()
+        
+    #     dose_num_list = chrome_driver.find_elements_by_css_selector(elem_css_selector)
+    #     print("vacc list contains {} vaccs".format(len(dose_num_list)))
+    # elem_css_selector = "div.tw-text-n700"
+    # address_list = None
+    # if chrome_driver.find_elements_by_css_selector(elem_css_selector)[1::3]:
+    #     address_list = chrome_driver.find_elements_by_css_selector(elem_css_selector)[1::3]
+    #     print("address list contains {} addresses".format(len(address_list)))
+    #     print("inner HTML for second address is {}".format(address_list[2].get_property("innerHTML")))
+    # elem_css_selector = "div.tw-p-5.tw-pt-3.tw-border-t.tw-border-n200 button"
+    # button_list = None
+    # if chrome_driver.find_elements_by_css_selector(elem_css_selector):
+    #     button_list = chrome_driver.find_elements_by_css_selector(elem_css_selector)
+    #     print("button list contains {} buttons".format(len(button_list)))
+    
+    # if dose_num_list and address_list and button_list:
+    #     for (title_elem, address_elem, button_elem) in zip(dose_num_list, address_list, button_list):
+    #         if "1st" in title_elem.text and "Downey" in address_elem.text:
+    #             print("1st and Downey found in title and address")
+    #         else:
+    #             print("doesnt contain match. iteration has {} and {}".format(title_elem.text, address_elem.text))
+            # if re.search("1st", title_elem.text) and re.search("Downey", address_elem.text):
+            #     button_elem.click()
+            #     print("1st dose in downey was found")
+            #     CHECKSUM += 1
+            #     sleep(LONGER_PAUSE)
+        
+    #             #...APPOINTMENT SELECT PAGE
+    #             elem_css_selector = 'h3'
+    #             if chrome_driver.find_elements_by_css_selector(elem_css_selector):
+    #                 CHECKSUM += 1
+    #                 elem_property = "innerHTML"
+    #                 appts_avail_text = chrome_driver.find_element_by_tag_name(elem_css_selector).get_property(elem_property)
+    #                 num_appts = int(appts_avail_text[0]) # appointments number count
+                
+    #                 curr_URL = chrome_driver.current_url # current window's URL
+    #                 print(curr_URL)
+                    
+    #                 # send text to notify appointments available to schedule
+    #                 if num_appts > 0:
+    #                     resp = requests.post(
+    #                         TEXTBELT_URL, {
+    #                             'phone': PHONE_NUM,
+    #                             'message': curr_URL,
+    #                             'key': TXT_API_KEY,
+    #                         }
+    #                     )
+    #                     print(HIT_RESPONSE)
+    #                     CHECKSUM += 1
+    #                 else:
+    #                     print(MISS_RESPONSE)
+    #                     CHECKSUM += 1
+    #                 sleep(LONGER_PAUSE)
+    # else:
+    #     print("pre appts page had issue")
+    
+    # print("CHECKSUM is {}".format(CHECKSUM))
+    # assert CHECKSUM >= 5
     
 if __name__ == "__main__":
     check_availability()
